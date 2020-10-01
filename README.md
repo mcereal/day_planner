02609d21a86016a8b66bda76396aee28a855c702
mcereal <mpbcurtis@gmail.com>
2020-09-30 20:52:54 -0400

    complete except for button icons

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # day_planner
+
+https://mcereal.github.io/day_planner/
+
+This application is a Javascript/Jquery based day planner
+
+This application displays blocks of time in a business day.
+
+Hour blocks in the past are greyed out, present hour blocks are red, and future hour blocks are green
+
+A user can enter text into the hour blocks and click save to save the event
+
+User saved events will persits after browser refreshes and closes
--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
+  //ids of each input
   const idsOfInputBlocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  const buttonInputIds = {
+    9: "9AMSubmitBtn",
+    10: "10AMSubmitBtn",
+    11: "11AMSubmitBtn",
+    12: "12PMSubmitBtn",
+    13: "1PMSubmitBtn",
+    14: "2PMSubmitBtn",
+    15: "3PMSubmitBtn",
+    16: "4PMSubmitBtn",
+    17: "5PMSubmitBtn",
+  };
+
+  //displays current date at the top of the page
   $(currentDay).text(moment().format("dddd, MMMM Do"));
+
+  //loops through ids and compares them to the current hour anc changing their color
   for (i in idsOfInputBlocks) {
     if (idsOfInputBlocks[i] < moment().hour()) {
       $(`#${idsOfInputBlocks[i]}`).addClass("past");
@@ -8,4 +24,16 @@ $(document).ready(function () {
       $(`#${idsOfInputBlocks[i]}`).addClass("present");
     } else $(`#${idsOfInputBlocks[i]}`).addClass("future");
   }
+
+  for (const [key, value] of Object.entries(localStorage)) {
+    $(`#${key}`).val(`${value}`);
+  }
+
+  $("#calendarBlock").on("click", function (event) {
+    for (const [key, value] of Object.entries(buttonInputIds)) {
+      if (value === event.target.id) {
+        localStorage.setItem(`${key}`, $(`#${key}`).val());
+      }
+    }
+  });
 });
